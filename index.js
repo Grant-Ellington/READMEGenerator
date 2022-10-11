a1596cb3cbb8ebd86d1af55cba21e720a1b52657
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-11 15:58:43 -0500

    added questions array

--- index.js
+++ index.js
@@ -1,7 +1,81 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'synopsis',
+        message: 'Eneter a short synopsis of you projecct?',
+    },
+    {
+        type: 'input',
+        name: 'tech1',
+        message: 'What is one technology that you used?',
+    },
+    {
+        type: 'input',
+        name: 'tech2',
+        message: 'What is another technology you used?',
+    },
+    {
+        type: 'input',
+        name: 'tech3',
+        message: 'What is another technology you used?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+        },
+    {
+        type: 'input',
+        name: 'usage',
+        message:'What are your Usage Insrunctions?',
+        },
+    {
+        type: 'input',
+        name: 'contGuide',
+        message: 'Who are your constribution guidleines?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are your test?'  
+
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license do you need?',
+        choices: [
+            'MIT',
+            'GNU GLPv3',
+            '',
+        ]
+    },
+    {
+        type: 'input',
+        name: 'gitUser',
+        message: 'What are your GitHub user name?'  
+
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What are your email address?'  
+
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
