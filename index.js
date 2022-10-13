600b3c8a668bff8b61c18b27286b8d0a43cd2566
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-13 11:44:10 -0500

    added license functions

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-    fs.writeFile('README1.md', data, (err) =>
+    fs.writeFile('dist/README.md', data, (err) =>
     err ? console.log(err) :console.log('success'))
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,10 +2,10 @@
 //If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license === 'MIT'){
-    var badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    var badge = '(https://img.shields.io/badge/License- MIT-yellow.svg)'
     return badge
 } else if (license === 'GNU GLPv3'){
-    var badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    var badge = '(https://img.shields.io/badge/License-GPLv3-blue.svg)'
     return badge
 } else {
     var badge = "";
@@ -17,10 +17,10 @@ function renderLicenseBadge(license) {
 //If there is no license, return an empty string
 function renderLicenseLink(license) { 
   if(license === 'MIT'){
-  var link = 'https://opensource.org/licenses/MIT'
+  var link = '(https://opensource.org/licenses/MIT)'
   return link
 } else if (license === 'GNU GLPv3'){
-  var link = 'https://www.gnu.org/licenses/gpl-3.0'
+  var link = '(https://www.gnu.org/licenses/gpl-3.0)'
   return link
 } else {
   return ""}
@@ -29,10 +29,10 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license === 'MIT'){
-    var licSec = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    var licSec = `[![License: MIT]${renderLicenseBadge(license)}]${renderLicenseLink(license)}`
     return licSec
 } else if (license === 'GNU GLPv3'){
-    var LicSec= '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    var licSec = `[![License: GPL v3]${renderLicenseBadge(license)}]${renderLicenseLink(license)}`
     return licSec
 } else {
     var licSec= "";
@@ -43,53 +43,54 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data,licSec) {
     return `## Table of Contents
-    * [License](*License)
-    * [Title](*Title)
-    * [Description](*Description)
-    * [Installation](*Installation)
-    * [Usage](*Usage)
-    * [Contribution](*Contribution)
-    * [Test](*Test)
-    * [Technologies](*Technologies)
-    * [Questions](*Questions)
-    
-    ## License
-    
-    ${licSec}
-    # ${data.title} 
-    ### ${data.name}
-    ## Description
-    
-    ${data.synopsis}
-    
-    ## Installation
-    
-    ${data.installation}
-    
-    ## Usage Information
-    
-    ${data.usage}
-    
-    ## Contribution Guidlines
-    
-    ${data.contGuide}
-    
-    ## Test
-    
-    ${data.test}
-    
-    
-    ## Techonologies
-    Project is created with:
-    * ${data.tech1}
-    * ${data.tech2}
-    * ${data.tech3}
-    ## Set-up
-    
-    
-    ## Questions
-    ${data.gitUser}
-    [${data.email}](${data.email})`
+  * [License](*License)
+  * [Title](*Title)
+  * [Description](*Description)
+  * [Installation](*Installation)
+  * [Usage](*Usage)
+  * [Contribution](*Contribution)
+  * [Test](*Test)
+  * [Technologies](*Technologies)
+  * [Questions](*Questions)
+  
+  ## License
+  
+  ${licSec}
+
+  # ${data.title} 
+  ### ${data.name}
+  ## Description
+  
+  ${data.synopsis}
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage Information
+  
+  ${data.usage}
+  
+  ## Contribution Guidlines
+  
+  ${data.contGuide}
+  
+  ## Test
+  
+  ${data.test}
+  
+  
+  ## Techonologies
+  Project is created with:
+  * ${data.tech1}
+  * ${data.tech2}
+  * ${data.tech3}
+  ## Set-up
+  
+  
+  ## Questions
+  ${data.gitUser}
+  [${data.email}](${data.email})`
 
 
 
